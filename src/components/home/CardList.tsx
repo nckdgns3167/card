ac49f02e15973763d3dc1369b61c4fcd4f6f8af8
nckdgns3167 <nckdgns3167@naver.com>
2025-01-08 22:22:56 +0900

    카드 목록 데이터 불러오기 (firebase) - 쿼리 커서 페이징 처리 포함

---
+++ src/components/home/CardList.tsx
@@ -0,0 +1,64 @@
+import React, { useCallback } from 'react'
+import ListRow from '@shared/ListRow'
+import { getCards } from '@remote/card'
+import { useInfiniteQuery } from 'react-query'
+import { flatten } from 'lodash'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+const CardList = () => {
+  const {
+    data,
+    hasNextPage = false,
+    fetchNextPage,
+    isFetching,
+  } = useInfiniteQuery(
+    ['cards'],
+    ({ pageParam }) => {
+      // console.log('pageParam', pageParam)
+      return getCards(pageParam)
+    },
+    {
+      getNextPageParam: (snapshot) => {
+        // console.log('snapshot', snapshot)
+        return snapshot.lastVisible // 맨 마지막 아이템을 리턴 -> 다음 페이지 호출할 때 이용 가능.
+      },
+    },
+  )
+
+  // console.log('data', data)
+  // console.log('hasNextPage', hasNextPage)
+
+  const loadMore = useCallback(() => {
+    if (!hasNextPage || isFetching) return
+    fetchNextPage()
+  }, [fetchNextPage, hasNextPage, isFetching])
+
+  if (data == null) {
+    return null
+  }
+
+  const cards = flatten(data?.pages.map(({ items }) => items))
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={cards.length}
+        hasMore={hasNextPage}
+        loader={<></>}
+        next={loadMore}
+      >
+        {cards.map((card, index) => (
+          <ListRow
+            key={card.id}
+            contents={
+              <ListRow.Texts title={`${index + 1}위`} subTitle={card.name} />
+            }
+            right={card.payback && <div>{card.payback}</div>}
+            withArrow
+          />
+        ))}
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+export default CardList
