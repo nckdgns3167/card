0c4e6655c3cb48fa929ead43434aca2566c7b388
nckdgns3167 <nckdgns3167@naver.com>
2025-01-08 22:02:58 +0900

    광고배너 데이터 불러오기 (firebase)

---
+++ src/remote/adBanner.ts
@@ -0,0 +1,17 @@
+import { collection, getDocs } from 'firebase/firestore'
+
+import { store } from './firebase'
+
+import { COLLECTIONS } from '@/constants/constants'
+import { AdBanner } from '@models/card'
+
+export async function getAdBanners() {
+  const adBannerSnapshot = await getDocs(
+    collection(store, COLLECTIONS.AD_BANNER),
+  )
+
+  return adBannerSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as AdBanner),
+  }))
+}
