15b8297c00a6750cdd0b93d6f92a006f59041a04
nckdgns3167 <nckdgns3167@naver.com>
2025-01-08 22:25:32 +0900

    firebase 에 mock 데이터 엎기 버튼 구현

---
+++ src/pages/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardListAddButton from '@components/test/CardListAddButton'
+import AdBannerListAddButton from '@components/test/AdBannerListAddButton'
+
+const TestPage = () => {
+  return (
+    <div>
+      <CardListAddButton />
+      <AdBannerListAddButton />
+    </div>
+  )
+}
+
+export default TestPage
