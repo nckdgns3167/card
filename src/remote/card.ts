ea8ac46233910382dd0c851659d3d0e3d19d7dd9
nckdgns3167 <nckdgns3167@naver.com>
2025-01-09 22:13:30 +0900

    단일 카드 정보 조회 함수 구현

--- src/remote/card.ts
+++ src/remote/card.ts
@@ -5,6 +5,8 @@ import {
   query,
   limit,
   startAfter,
+  doc,
+  getDoc,
 } from 'firebase/firestore'
 
 import { store } from './firebase'
@@ -34,4 +36,13 @@ export async function getCards(pageParam?: QuerySnapshot<Card>) {
   }))
 
   return { items, lastVisible }
+}
+
+export async function getCard(id: string) {
+  // 우리 앱에 잇는 문서를 찾을건데, 어떤 문서를? 카드라는 컬렉션에서 id를 가진 문서를...
+  const snapshot = await getDoc(doc(store, COLLECTIONS.CARD, id))
+  return {
+    id,
+    ...(snapshot.data() as Card),
+  }
 }
